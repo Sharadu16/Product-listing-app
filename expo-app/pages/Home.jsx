8b822a446c2ac2c2e3577964f5bdd26facf48d88
Sharadinduprakash <Sharadyadav2786@gmail.com>
2023-08-24 18:52:23 +0530

    searching functionality completed also work on filter functionality which is also done

--- expo-app/pages/Home.jsx
+++ expo-app/pages/Home.jsx
@@ -7,8 +7,10 @@ import {
   TextInput,
   TouchableOpacity,
   FlatList,
+  Modal,
+  Pressable,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import Product from "../components/Product";
 
@@ -18,6 +20,9 @@ const deviceHeight = Dimensions.get("window").height;
 const Home = () => {
   const [input, setInput] = useState("");
   const [product, setProduct] = useState([]);
+  const [oldData, setOldData] = useState([]);
+  const searchRef = useRef();
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -27,16 +32,24 @@ const Home = () => {
     try {
       let res = await fetch("https://dummyjson.com/products");
       let data = await res.json();
-    //   console.log("data is here------->>>>>>>>>>>>",data);
+      //   console.log("data is here------->>>>>>>>>>>>",data);
       setProduct(data.products);
+      setOldData(data.products);
     } catch {
       console.log(err);
     }
   };
 
   const searchProducts = (text) => {
-   
-  }
+    if (text == "") {
+      setProduct(oldData);
+    } else {
+      let filterData = product.filter((item) => {
+        return item.title.toLowerCase().indexOf(text.toLowerCase()) > -1;
+      });
+      setProduct(filterData);
+    }
+  };
 
   return (
     <View>
@@ -56,25 +69,114 @@ const Home = () => {
         >
           This is An Ecommerce App
         </Text>
-        <View style={styles.searchBox}>
-          <TextInput
-            placeholder="Search Your products"
-            placeholderTextColor="white"
-            onChangeText={prev => {setInput(prev); searchProducts(prev)}}
-            value={input}
-            style={{
-              color: "white",
-              paddingHorizontal: 5,
-              fontSize: 16,
-              width: 210,
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <View style={styles.searchBox}>
+            <TextInput
+              placeholder="Search Your products"
+              placeholderTextColor="white"
+              onChangeText={(prev) => {
+                setInput(prev);
+                searchProducts(prev);
+              }}
+              value={input}
+              style={{
+                color: "white",
+                paddingHorizontal: 5,
+                fontSize: 16,
+                width: 210,
+              }}
+            />
+            {input == "" ? (
+              <TouchableOpacity onPress={() => {}}>
+                <AntDesign name="search1" size={22} color="white" />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                onPress={() => {
+                  setInput("");
+                  searchProducts("");
+                  searchRef.current.clear();
+                }}
+              >
+                <AntDesign name="close" size={22} color="white" />
+              </TouchableOpacity>
+            )}
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              setModalVisible(!modalVisible);
             }}
-          />
-          <TouchableOpacity onPress={() => {}}>
-            <AntDesign name="search1" size={22} color="white" />
+          >
+            <AntDesign
+              name="filter"
+              size={42}
+              color="white"
+              style={{ left: 10 }}
+            />
           </TouchableOpacity>
         </View>
+        {/* open modal here to filter the products  */}
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <TouchableOpacity
+                  onPress={() => {
+                    let sortData = product.sort((a,b) => a.title > b.title ? 1 : -1);
+                    setProduct(sortData);
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.modalText}>Sort By Name</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    let sortData = product.sort((a,b) => a.price - b.price);
+                    setProduct(sortData);
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.modalText}>Price Low To high</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    let sortData = product.sort((a,b) => b.price - a.price);
+                    setProduct(sortData);
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.modalText}>Price High To Low</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    let sortData = product.sort((a,b) => b.rating - a.rating);
+                    setProduct(sortData);
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.modalText}>Sort By Rating</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        {/* modal has closed here   */}
         <FlatList
-          style={{marginVertical:10}}
+          style={{ marginVertical: 10 }}
           showsVerticalScrollIndicator={false}
           data={product}
           renderItem={({ item }) => <Product item={item} />}
@@ -97,7 +199,7 @@ const styles = StyleSheet.create({
     height: deviceHeight,
   },
   searchBox: {
-    width: deviceWidth / 1.08,
+    width: deviceWidth / 1.26,
     height: 56,
     borderWidth: 1,
     borderRadius: 7,
@@ -110,5 +212,28 @@ const styles = StyleSheet.create({
   imageBox: {
     width: deviceWidth,
     height: "100%",
+  },
+
+  // style for modal view //
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+    paddingHorizontal: 90,
+    paddingVertical: 20,
+    alignItems: "center",
+    
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
